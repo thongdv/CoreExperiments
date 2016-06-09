086c1be6b7111ee3255ba6e228f6b615c1f3c97b
SivaTeja <sivateja890@gmail.com>
2016-06-09 11:39:42 +0530

    updated
    updated

--- FindDuplicateElementsInArray.java
+++ FindDuplicateElementsInArray.java
@@ -3,6 +3,7 @@ import java.util.Set;
 
 public class FindDuplicateElementsInArray {
 
+	//best method
 	public static void main(String[] args) {
 		String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
 		Set<String> set = new HashSet<String>();
@@ -12,4 +13,23 @@ public class FindDuplicateElementsInArray {
 			}
 		}
 	}
+	
+	//bruteforcemethod
+	/*
+	public static void main(String[] args) 
+    {
+        String[] strArray = {"abc", "def", "mno", "xyz", "pqr", "xyz", "def"};
+ 
+        for (int i = 0; i < strArray.length-1; i++)
+        {
+            for (int j = i+1; j < strArray.length; j++)
+            {
+                if( (strArray[i].equals(strArray[j])) && (i != j) )
+                {
+                    System.out.println("Duplicate Element is : "+strArray[j]);
+                }
+            }
+        }
+    }   
+    */ 
 }
